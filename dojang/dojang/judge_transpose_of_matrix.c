ca6e8ffac25b3b517d28db8f5f1a7e2da4765f4e
jun097kim <jun097kim@gmail.com>
2017-09-25 18:04:57 +0900

    p. 492 Transpose of matrix

--- dojang/dojang/judge_transpose_of_matrix.c
+++ dojang/dojang/judge_transpose_of_matrix.c
@@ -15,7 +15,7 @@ int main() {
 
 	for (int i = 0; i < row; i++) {
 		for (int j = 0; j < col; j++) {
-			if (i != j) {
+			if (i < j) {
 				int tmp = matrix[i][j];
 				matrix[i][j] = matrix[j][i];
 				matrix[j][i] = tmp;
